886610ea7d977a260d32caaab86bfa0454587a94
Arkadiusz Rybka <arekrybka@gmail.com>
2017-04-17 12:41:42 +0200

    Zmiana wyświetlania wyników wyszukiwania

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('.countries');
+var table = $('.table');
+var results = $('.results');
+var table = $('.table');
 var country = $('.country');
 var capital = $('.capital');
 var landArea = $('.land-area');
@@ -7,14 +10,13 @@ var population = $('.population');
 var language = $('.language');
 var currency = $('.currency');
 
-countriesList.hide();
+hideElements();
 
 $('#search').click(searchCountries);
 
 function searchCountries() {
 	var countryName = $('#country-search').val();
 	if (!countriesList) countryName = 'Poland';
-
 	$.ajax({
 		url: url + countryName,
 		method: 'GET',
@@ -23,27 +25,58 @@ function searchCountries() {
 };
 
 function showCountriesList(resp) {
-
-	var minus = resp.length - 5;
-	resp.splice(5, minus);
-	
 	resp.forEach(function(element) {
-		
-		$('.info').hide();
-		countriesList.show();
+		var state = element.name.toLowerCase();
+		var searchStr = $('#country-search').val();
+		if (state.includes(searchStr)) {
+			$('<li>').addClass('country-name').text(element.name).appendTo('.results');
+		}
+	});
+	$('.info').hide();
+	countriesList.show();
+	$('#clear').show();
+	selectCountryName();
+};
 
+function selectCountryName() {
+	$('li.country-name').on('click', function() {
+		var countryName = $(this).text();
+		$.ajax({
+			url: url + countryName,
+			method: 'GET',
+			success: showCountryDetails
+		});
+	});
+};
+
+function showCountryDetails(resp) {
+	$('td').remove();
+	resp.forEach(function(element) {
 		$('<td>').text(element.name).appendTo(country);
 		$('<td>').text(element.capital).appendTo(capital);
 		$('<td>').text(element.area).appendTo(landArea);
 		$('<td>').text(element.population).appendTo(population);
 		$('<td>').text(element.languages).appendTo(language);
 		$('<td>').text(element.currencies[0]).appendTo(currency);
-
 	});
+	showElements();
+
 };
 
 $('#clear').on('click', function() {
-	$('td').remove();
-	countriesList.hide();
+	hideElements();
+	$('li').remove();
 	$('#country-search').val('').focus();
 });
+
+function hideElements() {
+	countriesList.hide();
+	table.hide();
+	$('#clear').hide();
+};
+
+function showElements() {
+	countriesList.show();
+	table.show();
+	$('#clear').show();
+};
